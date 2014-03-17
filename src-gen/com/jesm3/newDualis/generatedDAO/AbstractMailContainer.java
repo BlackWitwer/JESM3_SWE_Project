1476aa288cf6f66684e80508929a22340ba9d773
eva9876 <eva.leonie@gmx.de>
2014-03-17 09:13:19 +0100

    Hinzufügen von messageNumber in generierte Mail Klasse

--- src-gen/com/jesm3/newDualis/generatedDAO/AbstractMailContainer.java
+++ src-gen/com/jesm3/newDualis/generatedDAO/AbstractMailContainer.java
@@ -16,6 +16,7 @@ public class AbstractMailContainer {
     private Boolean attachment;
     private Boolean seen;
     private Boolean html;
+    private Integer messageNumber;
 
     public AbstractMailContainer() {
     }
@@ -24,7 +25,7 @@ public class AbstractMailContainer {
         this.id = id;
     }
 
-    public AbstractMailContainer(Long id, String from, String fromComplete, String to, String subject, String text, java.util.Date date, Boolean attachment, Boolean seen, Boolean html) {
+    public AbstractMailContainer(Long id, String from, String fromComplete, String to, String subject, String text, java.util.Date date, Boolean attachment, Boolean seen, Boolean html, Integer messageNumber) {
         this.id = id;
         this.from = from;
         this.fromComplete = fromComplete;
@@ -35,6 +36,7 @@ public class AbstractMailContainer {
         this.attachment = attachment;
         this.seen = seen;
         this.html = html;
+        this.messageNumber = messageNumber;
     }
 
     public Long getId() {
@@ -115,6 +117,14 @@ public class AbstractMailContainer {
 
     public void setHtml(Boolean html) {
         this.html = html;
+    }
+
+    public Integer getMessageNumber() {
+        return messageNumber;
+    }
+
+    public void setMessageNumber(Integer messageNumber) {
+        this.messageNumber = messageNumber;
     }
 
 }
--- src/com/jesm3/newDualis/persist/DualisDaoGenerator.java
+++ src/com/jesm3/newDualis/persist/DualisDaoGenerator.java
@@ -47,5 +47,6 @@ public class DualisDaoGenerator {
 		theMail.addBooleanProperty("attachment");
 		theMail.addBooleanProperty("seen");
 		theMail.addBooleanProperty("html");
+		theMail.addIntProperty("messageNumber");
 	}
 }
