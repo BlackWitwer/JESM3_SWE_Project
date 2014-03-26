ea28ace39b7de30362839a2688b4d31d88042aac
eva9876 <eva.leonie@gmx.de>
2014-03-26 14:44:22 +0100

    DaoGenerator um AbstractNote erweitert. AbstracNote-Klasse genertiert.

--- src/com/jesm3/newDualis/persist/DualisDaoGenerator.java
+++ src/com/jesm3/newDualis/persist/DualisDaoGenerator.java
@@ -13,6 +13,7 @@ public class DualisDaoGenerator {
 		Schema theSchema = new Schema(3, "com.jesm3.newDualis.generatedDAO");
 		
 		addVorlesung(theSchema);
+		addNoten(theSchema);
 		
 		File theFile = new File("./src-gen");
 		if (!theFile.exists()) {
@@ -30,5 +31,13 @@ public class DualisDaoGenerator {
 		theVorlesung.addDateProperty("uhrzeitVon");
 		theVorlesung.addDateProperty("uhrzeitBis");
 		theVorlesung.addStringProperty("raum");
+	}
+	
+	private static void addNoten(Schema aSchema) {
+		Entity theNote = aSchema.addEntity("AbstractNote");
+		theNote.addIdProperty().autoincrement();
+		theNote.addStringProperty("titel");
+		theNote.addStringProperty("note");
+		theNote.addStringProperty("credits");
 	}
 }
